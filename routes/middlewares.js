b3135e24e5bb68e633bb8baf7dceade110701f3e
yeonho Park <whwlsvy12@naver.com>
2018-12-06 00:47:18 +0900

    isNotLoggedIn logic check

--- routes/middlewares.js
+++ routes/middlewares.js
@@ -1,4 +1,5 @@
 exports.isLoggedIn = (req, res, next) => {
+  console.log("isLoggedIn");
   if (req.isAuthenticated()) {
     next();
   } else {
@@ -7,7 +8,8 @@ exports.isLoggedIn = (req, res, next) => {
 };
 
 exports.isNotLoggedIn = (req, res, next) => {
-  if (req.isAuthenticated()) {
+  console.log("isNotLoggedIn");
+  if (!req.isAuthenticated()) {
     // isAuthenticated()는 passport가 추가해줌. logi여부를 확인할 수 있다.
     next();
   } else {
