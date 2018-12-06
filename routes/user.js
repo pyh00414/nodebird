eb82bdd8f52ccdad4ec5de4afb32e1918a07d713
yeonho Park <whwlsvy12@naver.com>
2018-12-06 22:41:47 +0900

    correct misspell

--- routes/user.js
+++ routes/user.js
@@ -4,16 +4,16 @@ const router = express.Router();
 const { isLoggedIn } = require("./middlewares");
 const { User } = require("../models");
 
-router.post("/:id/follow", isLoggedIn),
-  async (req, res, next) => {
-    try {
-      const user = await User.find({ where: { id: req.user.id } });
-      await user.addFollowing(parseInt(req.params.id, 10));
-      res.send("success");
-    } catch (error) {
-      console.error(error);
-      next(error);
-    }
-  };
+router.post("/:id/follow", isLoggedIn, async (req, res, next) => {
+  try {
+    const user = await User.find({ where: { id: req.user.id } });
+
+    await user.addFollowing(parseInt(req.params.id, 10));
+    res.send("success");
+  } catch (error) {
+    console.error(error);
+    next(error);
+  }
+});
 
 module.exports = router;
